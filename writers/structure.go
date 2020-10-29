8d4cc4b2ec7305edd761cce163c7061aebc50374
prashant sharma <prashant.sharma@enquero.com>
2020-10-29 22:22:16 +0530

    added logic to parse constant json files

--- writers/structure.go
+++ writers/structure.go
@@ -1,5 +1,14 @@
 package writers
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/Prashant-sharma3012/api-generator/models"
+)
+
 // responsible for creating folder structure befotre writers start writing to them
 
 // for now hardcoding thinsg
@@ -12,13 +21,65 @@ type Folders struct {
 type FolderStructure struct {
 	RootFolders []Folders `json:"rootFolders"`
 	RootFiles   []string  `json:"rootFiles"`
+	ProjectName models.Project
+}
+
+type FolderToTemplate struct {
+	FilePath     string `json:"filePath"`
+	TemplateName string `json:"templateName"`
+}
+
+type FolderToTemplateData struct {
+	Data []FolderToTemplate `json:"data"`
 }
 
 func (f *FolderStructure) CreateEmptyStructure() {
+	// make folder with project name
+
+	// create empty root files
+
+	// create empty folders
+
+}
+
+func WriteStaticTemplates(templateMap map[string]string, basePath string) {
 
 }
 
 func ParseFolderStructure() *FolderStructure {
+	content, err := ioutil.ReadFile("constants/folderStructure.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	return &FolderStructure{}
+	fs := FolderStructure{}
+
+	err = json.Unmarshal(content, &fs)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return &fs
+}
+
+func ParseFileToFolderMap() map[string]string {
+	var pathToTemplateMap map[string]string
+
+	content, err := ioutil.ReadFile("constants/folderStructure.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fd := FolderToTemplateData{}
+
+	err = json.Unmarshal(content, &fd)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	for _, record := range fd.Data {
+		pathToTemplateMap[record.FilePath] = record.TemplateName
+	}
+
+	return pathToTemplateMap
 }
